cac2cfe048b76e631162a22dafc27a5f810978cd
Glitch (hello-express) <none>
2018-12-05 21:20:49 +0000

    📰📚 Checkpoint ./package.json:2247223/40 ./bot.js:2247223/2974

--- bot.js
+++ bot.js
@@ -1,22 +1,83 @@
-// server.js
-// where your node app starts
+const tmi = require('tmi.js')
 
-// init project
+// Valid commands start with:
+let commandPrefix = '!'
+// Define configuration options:
+let opts = {
+  identity: {
+    username: process.env.BOT_USERNAME,
+    password: process.env.BOT_OAUTH 
+  },
+  channels: [
+    //process.env.CHANNEL
+    'mauerbac'
+  ]
+}
+
+// Function called when the "echo" command is issued:
+function echo (target, context, params) {
+  // If there's something to echo:
+  if (params.length) {
+    // Join the params into a string:
+    const msg = params.join(' ')
+    // Send it back to the correct place:
+    client.say(target, msg)
+  } else { // Nothing to echo
+    console.log(`* Nothing to echo`)
+  }
+}
+
+// Create a client with our options:
+let client = new tmi.client(opts)
+
+// Register our event handlers (defined below):
+client.on('message', onMessageHandler)
+client.on('connected', onConnectedHandler)
+client.on('disconnected', onDisconnectedHandler)
+
+// Connect to Twitch:
+client.connect()
+
+// Called every time a message comes in:
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return } // Ignore messages from the bot
+
+  // This isn't a command since it has no prefix:
+  if (msg.substr(0, 1) !== commandPrefix) {
+    console.log(`[${target} (${context['message-type']})] ${context.username}: ${msg}`)
+    return
+  }
+
+  // Split the message into individual words:
+  const parse = msg.slice(1).split(' ')
+  // The command name is the first (0th) one:
+  const commandName = parse[0]
+  // The rest (if any) are the parameters:
+  const params = parse.splice(1)
+
+  // If the command is known, let's execute it:
+  if (commandName == 'echo') {
+    echo(target, context, params)
+    console.log(`* Executed ${commandName} command for ${context.username}`)
+  } else {
+    console.log(`* Unknown command ${commandName} from ${context.username}`)
+  }
+}
+
+// Called every time the bot connects to Twitch chat:
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`)
+}
+
+// Called every time the bot disconnects from Twitch:
+function onDisconnectedHandler (reason) {
+  console.log(`Disconnected: ${reason}`)
+  process.exit(1)
+}
+
 const express = require('express');
 const app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Listening on port ', + listener.address().port);
 });
