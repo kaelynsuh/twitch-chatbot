dc8d69eeeffecbaae71b9b35fc4aa9c433ba43e7
Glitch (twitch-chatbot) <none>
2019-01-02 20:25:39 +0000

    🍹💅 Checkpoint ./bot.js:2247223/3854

--- bot.js
+++ bot.js
@@ -1,76 +1,67 @@
-const tmi = require('tmi.js')
+const tmi = require('tmi.js');
 
 // Valid commands start with:
-let commandPrefix = '!'
+const commandPrefix = '!';
 // Define configuration options:
-let opts = {
+const opts = {
   identity: {
     username: process.env.BOT_USERNAME,
-    password: process.env.OAUTH_TOKEN 
+    password: process.env.OAUTH_TOKEN
   },
   channels: [
     process.env.CHANNEL_NAME
   ]
+};
+
+// Create a client with our options:
+const client = new tmi.Client(opts);
+
+// Register our event handlers (defined below):
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+client.on('disconnected', onDisconnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in:
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+
+  // This isn't a command since it has no prefix:
+  if (msg[0] !== commandPrefix) {
+    console.log(`[${target} (${context['message-type']})] ${msg}`);
+    return;
+  }
+
+  // Split the message into individual words:
+  const parse = msg.slice(1).split(' ');
+  // The command name is the first (0th) one:
+  const commandName = parse[0].toLowerCase();
+
+  // If the command is known, let's execute it:
+  if (commandName === 'dice') {
+    const num = rollDice();
+    client.say(target, `You rolled a ${num}`);
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
 }
 
 // Function called when the "dice" command is issued:
 function rollDice () {
   const sides = 6;
-  return Math.floor(Math.random() * sides) + 1
- 
-}
-
-// Create a client with our options:
-let client = new tmi.client(opts)
-
-// Register our event handlers (defined below):
-client.on('message', onMessageHandler)
-client.on('connected', onConnectedHandler)
-client.on('disconnected', onDisconnectedHandler)
-
-// Connect to Twitch:
-client.connect()
-
-// Called every time a message comes in:
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return } // Ignore messages from the bot
-
-  // This isn't a command since it has no prefix:
-  if (msg.substr(0, 1) !== commandPrefix) {
-    console.log(`[${target} (${context['message-type']})] ${msg}`)
-    return
-  }
-
-  // Split the message into individual words:
-  const parse = msg.slice(1).split(' ')
-  // The command name is the first (0th) one:
-  const commandName = parse[0]
-
-  // If the command is known, let's execute it:
-  if (commandName == 'dice') {
-    const num = rollDice()
-    client.say(target, `You rolled a ${num}`)
-    console.log(`* Executed ${commandName} command`)
-  } else {
-    console.log(`* Unknown command ${commandName}`)
-  }
+  return Math.floor(Math.random() * sides) + 1;
 }
 
 // Called every time the bot connects to Twitch chat:
 function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`)
+  console.log(`* Connected to ${addr}:${port}`);
 }
 
 // Called every time the bot disconnects from Twitch:
 function onDisconnectedHandler (reason) {
-  console.log(`Disconnected: ${reason}`)
-  process.exit(1)
+  console.log(`Disconnected: ${reason}`);
+  process.exit(1);
 }
-
-const express = require('express')
-const app = express();
-
-var listener = app.listen(process.env.PORT, function() {
-  console.log('Listening on port ', + listener.address().port)
-  app.get('/', (req, res) => res.send('Hello World!'))
-});
