ea8fb0582db2a84d9e5940ebdd27e668f623d106
Glitch (twitch-chatbot) <none>
2019-01-02 20:47:33 +0000

    💅📺 Checkpoint ./bot.js:2247223/3147

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 const tmi = require('tmi.js');
 
-// Valid commands start with:
-const commandPrefix = '!';
-// Define configuration options:
+// Define configuration options
 const opts = {
   identity: {
     username: process.env.BOT_USERNAME,
@@ -13,55 +11,41 @@ const opts = {
   ]
 };
 
-// Create a client with our options:
+// Create a client with our options
 const client = new tmi.Client(opts);
 
-// Register our event handlers (defined below):
+// Register our event handlers (defined below)
 client.on('message', onMessageHandler);
 client.on('connected', onConnectedHandler);
-client.on('disconnected', onDisconnectedHandler);
 
 // Connect to Twitch:
 client.connect();
 
-// Called every time a message comes in:
+// Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
+  // if (self) { return; } // Ignore messages from the bot
 
-  // This isn't a command since it has no prefix:
-  if (msg[0] !== commandPrefix) {
-    console.log(`[${target} (${context['message-type']})] ${msg}`);
-    return;
-  }
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
 
-  // Split the message into individual words:
-  const parse = msg.slice(1).split(' ');
-  // The command name is the first (0th) one:
-  const commandName = parse[0].toLowerCase();
-
-  // If the command is known, let's execute it:
-  if (commandName === 'dice') {
+  // If the command is known, let's execute it
+  if (commandName === '!dice') {
     const num = rollDice();
-    client.say(target, `You rolled a ${num}`);
+    //client.say(target, `You rolled a ${num}`);
+    client.say(target, '!dice');
     console.log(`* Executed ${commandName} command`);
   } else {
     console.log(`* Unknown command ${commandName}`);
   }
 }
 
-// Function called when the "dice" command is issued:
+// Function called when the "dice" command is issued
 function rollDice () {
   const sides = 6;
   return Math.floor(Math.random() * sides) + 1;
 }
 
-// Called every time the bot connects to Twitch chat:
+// Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
-}
-
-// Called every time the bot disconnects from Twitch:
-function onDisconnectedHandler (reason) {
-  console.log(`Disconnected: ${reason}`);
-  process.exit(1);
 }
